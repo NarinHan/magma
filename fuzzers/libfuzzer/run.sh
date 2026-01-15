027f865ffc2900cc648c2191dba52c006e83484d
NarinHan <lynniepro21@gmail.com>
2026-01-15 15:53:22 +0900

    Modify saving the libfuzzer stdout as a comment

--- fuzzers/libfuzzer/run.sh
+++ fuzzers/libfuzzer/run.sh
@@ -16,5 +16,4 @@ mkdir -p "$SHARED/findings"
 "$OUT/$PROGRAM" -rss_limit_mb=100 \
 	-fork=1 -ignore_timeouts=1 -ignore_crashes=1 -ignore_ooms=1 \
 	-artifact_prefix="$SHARED/findings/" $FUZZARGS \
-    "$TARGET/corpus/$PROGRAM" $ARGS \
-	&> "$SHARED/fuzzer_stdout.log"
+    "$TARGET/corpus/$PROGRAM" $ARGS # &> "$SHARED/fuzzer_stdout.log"
