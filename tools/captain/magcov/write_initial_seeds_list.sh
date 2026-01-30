4b09dfcf8546b0efa8d27e9b4cba38ffc6c5e49e
NarinHan <lynniepro21@gmail.com>
2026-01-30 09:30:57 +0900

    fix typo

--- tools/captain/magcov/write_initial_seeds_list.sh
+++ tools/captain/magcov/write_initial_seeds_list.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find $TARGET/corpus/$PROGRAM -maxdepth 1 -type f -prinf "%f\n" > /magma/magcov/initial_seeds.txt
+find $TARGET/corpus/$PROGRAM -maxdepth 1 -type f -printf "%f\n" > /magma/magcov/initial_seeds.txt
